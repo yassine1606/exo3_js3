eff33c0bc0db0473e3e934b67a86c7761ba95eae
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-21 12:30:43 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,115 @@
+/* exo 1
+for (let i = 0; i < 20; i++) {
+    if (i%3==0) {
+        continue;
+    }
+    console.log(i);
+}
+*/
+
+
+
+
+
+
+
+/* exo 2
+let user;
+user = prompt(`Ecrivez un nombre pair`)
+do {
+    user = prompt(`Ecrivez un nombre pair`)
+} while (user % 2 == 1);
+*/
+
+
+
+
+/* exo 3
+for (let index = 1; index < 10; index++) {
+    if (index==7) {
+        break;
+    }
+    console.log(index);
+}
+*/
+
+
+
+
+
+/* exo 4
+for (let i = 1; i < 10; i++) {
+    if (i%2==0) {
+        continue;
+    }
+    console.log(i);
+}
+*/
+
+
+
+
+
+
+
+/* exo 5
+for (let i = 1; i < 10; i++) {
+    if (i==6) {
+        break;
+        
+    }
+    console.log(i);
+    
+}
+console.log(` La boucle a été interrompue` );
+*/
+
+
+
+
+
+
+/*  exo 6
+for (let i = 1; i < 10; i++) {
+    
+    if (i%3==0) {
+        console.log(` L'itération a été sautée`);
+        continue;
+    }
+    console.log(i);
+}
+*/
+
+
+
+
+
+
+
+/* exo 7
+let bool = true;
+for (let i = 1 ; bool && i<=10; i++) {
+    console.log(i);
+    if (i==5 ) {
+        bool=false;
+        console.log(`La boucle a été interrompue`);
+    } 
+}
+
+
+
+
+
+/* exo 8 sauter et continuer j'ai pas compris*/
+/*
+for (let i = 1; i < 10; i++) {
+    
+    if (i%3==0) {
+        console.log(` L'itération a été sautée`);
+        continue;
+    }
+    console.log(i);
+}
+
+
+*/
